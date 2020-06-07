7ad23f21ddd163f08c84d1efa20eb08f4a14edc3
jason-json <jason.json@kakaocorp.com>
2020-06-07 23:03:56 +0900

    chore: rename build file

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from '@rollup/plugin-babel'
 export default {
   input: 'src/index.js',
   output: {
-    file: 'build/bundle.js',
+    file: 'build/index.js',
     format: 'cjs',
   },
   plugins: [babel({ babelHelpers: 'bundled' })],
