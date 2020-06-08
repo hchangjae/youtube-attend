24a3942b798a103f4281ba60235193b366332e36
jason-json <jason.json@kakaocorp.com>
2020-06-08 20:04:00 +0900

    Apply https

--- chrome/index.js
+++ chrome/index.js
@@ -1,4 +1,4 @@
-const url = 'http://youtube-attend.uniium.com/bundle.js'
+const url = 'https://youtube-attend.uniium.com/bundle.js'
 const onClick = () => {
   chrome.tabs.executeScript({
     code: `
